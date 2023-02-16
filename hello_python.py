e549109f19fb2fae599bb5a8e1803e911712fb79
Hitove <824499676@qq.com>
2023-02-16 23:04:14 +0800

    依托答辩

---
+++ hello_python.py
@@ -0,0 +1,15 @@
+import turtle
+
+turtle.pensize(8)
+turtle.pencolor('blue')
+
+turtle.forward(100)
+turtle.right(60)
+turtle.forward(100)
+turtle.right(80)
+
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+
+turtle.mainloop()
